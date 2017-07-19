7a3325747be8cde4fe81371a2501354b21042e82
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-07-18 17:30:56 -0700

    got the value of 4 into my divs and made a player alternator

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-console.log('working');
-
-
 $(() => {
   console.log('working 2');
 
-  for(let i=0; i <= 5; i++){
+  let playerAlternate = true;
+
+  for(let i=1; i <= 6; i++){
     const $div = $('<div/>').addClass('container-top');
+    $($div).attr('id', 'top' + [i]);
+    $($div).html(4);
     $('#row-top').append($div);
   };
 
 
-  for(let i=0; i <= 5; i++) {
+  for(let i=1; i <= 6; i++) {
     const $div2 = $('<div/>').addClass('container-bottom');
+    $($div2).attr('id', 'bottom' + [i]);
+    $($div2).html(4);
     $('#row-bottom').append($div2);
   }
+
+  const changePlayer = () => {
+    if(playerAlternate === true){
+      $('#player').text("Player Two's Turn");
+      playerAlternate = false;
+    }else {
+      $('#player').text("Player One's Turn");
+      playerAlternate = true
+    }
+
+  }
+
+
+
+
+
+
+
+
 
 
 
